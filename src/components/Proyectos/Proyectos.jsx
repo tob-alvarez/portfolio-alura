7be558cfa5305e88bb7fd38ea34158d105d7e1c2
Tobias Alvarez <tosal1099@gmail.com>
2023-08-28 19:02:32 -0300

    portfolio listo

--- src/components/Proyectos/Proyectos.jsx
+++ src/components/Proyectos/Proyectos.jsx
@@ -1,5 +1,7 @@
 import "./Proyectos.css";
 import fotoChallenge1 from "../../assets/Captura-C1.png";
+import fotoChallenge2 from "../../assets/alurageek.png";
+import fotoChallenge3 from "../../assets/Captura de pantalla 2023-08-28 185559.png";
 
 const Proyectos = () => {
   return (
@@ -43,21 +45,26 @@ const Proyectos = () => {
         <div className="proyecto mt-5">
           <div className="d-flex gap-5">
             <img
-              src={fotoChallenge1}
+              src={fotoChallenge2}
               alt="foto challenge alura #1"
               className="foto-challenge-1"
             />
             <div>
               <h3>Proyecto 2</h3>
-              <p className="descripcion-challenge">Proyecto 2</p>
+              <p className="descripcion-challenge">
+                Proyecto Alura Geek (e-commerce)
+              </p>
               <div>
                 <button className="boton-repo">
-                  <a href="#" target="blank">
+                  <a
+                    href="https://github.com/tob-alvarez/Alura-Geek-Tob"
+                    target="blank"
+                  >
                     Repositorio
                   </a>
                 </button>
                 <button className="boton-demo">
-                  <a href="#" target="blank">
+                  <a href="https://alura-geek-tob.vercel.app/" target="blank">
                     Ver Demo
                   </a>
                 </button>
@@ -68,21 +75,24 @@ const Proyectos = () => {
         <div className="proyecto mt-5">
           <div className="d-flex gap-5">
             <img
-              src={fotoChallenge1}
+              src={fotoChallenge3}
               alt="foto challenge alura #1"
               className="foto-challenge-1"
             />
             <div>
               <h3>Proyecto 3</h3>
-              <p className="descripcion-challenge">Challenge #1 Alura</p>
+              <p className="descripcion-challenge">Proyecto AluraFlix</p>
               <div>
                 <button className="boton-repo">
-                  <a href="#" target="blank">
+                  <a
+                    href="https://github.com/tob-alvarez/AluraFlix-Tob"
+                    target="blank"
+                  >
                     Repositorio
                   </a>
                 </button>
                 <button className="boton-demo">
-                  <a href="#" target="blank">
+                  <a href="https://tobflix.netlify.app/" target="blank">
                     Ver Demo
                   </a>
                 </button>
