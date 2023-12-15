9140d1ac0d60148e27c2489db8c03e1ccd50e47d
Tobias Alvarez <tosal1099@gmail.com>
2023-12-14 21:52:35 -0300

    cambios en proyects

--- src/components/Proyectos/Proyectos.jsx
+++ src/components/Proyectos/Proyectos.jsx
@@ -4,6 +4,7 @@ import fotoChallenge2 from "../../assets/alurageek.png";
 import fotoChallenge3 from "../../assets/toblfix.webp";
 import fotoChallenge4 from "../../assets/sisCom.webp";
 import fotoChallenge5 from "../../assets/xgames.png";
+import fotoChallenge6 from "../../assets/teloreservo.png";
 import { useContext } from "react";
 import { Contexto } from "../../context/Contexto";
 
@@ -12,46 +13,44 @@ const Proyectos = () => {
 
   return (
     <section
-      className="container-fluid d-flex flex-column align-items-center justify-content-center container1"
+      className=" d-flex flex-column align-items-center justify-content-center container1"
       style={
-        dark
-          ? { backgroundColor: "#202020", height: "100vh" }
-          : { backgroundColor: "#f2f2f2", height: "100vh" }
+        dark ? { backgroundColor: "#202020" } : { backgroundColor: "#f2f2f2" }
       }
     >
       <h2 className="text-center pb-4 nombreHome">Projects</h2>
-      <div className="d-flex flex-column justify-content-around w-100">
-        <div className="row w-100 ps-2 d-flex justify-content-center">
-          <div className="col-3">
-            <div className="card">
+      <div className="d-flex flex-column gap-2">
+        <div className="row w-100 ps-2 d-flex justify-content-center gap-2">
+          <div className="col-8 col-md-3">
+            <div className="cardProyecto">
               <img src={fotoChallenge4} alt="" className="fotoChallenge" />
             </div>
           </div>
-          <div className="col-3">
-            <div className="card">
-              <img src={fotoChallenge3} alt="" />
+          <div className="col-8 col-md-3">
+            <div className="cardProyecto">
+              <img src={fotoChallenge3} alt="" className="fotoChallenge" />
             </div>
           </div>
-          <div className="col-3">
-            <div className="card">
-              <img src={fotoChallenge2} alt="" />
+          <div className="col-8 col-md-3">
+            <div className="cardProyecto">
+              <img src={fotoChallenge2} alt="" className="fotoChallenge" />
             </div>
           </div>
         </div>
-        <div className="row w-100 ps-2 mt-5 d-flex justify-content-center">
-          <div className="col-3">
-            <div className="card">
-              <img src={fotoChallenge3} alt="" />
+        <div className="row w-100 ps-2 d-flex justify-content-center gap-2">
+          <div className="col-8 col-md-3">
+            <div className="cardProyecto">
+              <img src={fotoChallenge6} alt="" className="fotoChallenge" />
             </div>
           </div>
-          <div className="col-3">
-            <div className="card">
-              <img src={fotoChallenge5} alt="" />
+          <div className="col-8 col-md-3">
+            <div className="cardProyecto">
+              <img src={fotoChallenge5} alt="" className="fotoChallenge" />
             </div>
           </div>
-          <div className="col-3">
-            <div className="card">
-              <img src={fotoChallenge1} alt="" />
+          <div className="col-8 col-md-3">
+            <div className="cardProyecto">
+              <img src={fotoChallenge1} alt="" className="fotoChallenge" />
             </div>
           </div>
         </div>
